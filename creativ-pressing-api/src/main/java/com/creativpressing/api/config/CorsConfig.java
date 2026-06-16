b601597c4b8ef29fd2d1a50977e446ac89c5a43d
tapha21 <tvphvtall@gmail.com>
2026-06-16 12:39:36 +0000

    cors

--- creativ-pressing-api/src/main/java/com/creativpressing/api/config/CorsConfig.java
+++ creativ-pressing-api/src/main/java/com/creativpressing/api/config/CorsConfig.java
@@ -2,7 +2,6 @@ package com.creativpressing.api.config;
 
 import com.creativpressing.api.service.MediaStorageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,15 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     private final MediaStorageService mediaStorageService;
 
-    @Value("${app.cors.allowed-origins}")
-    private String[] origins;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(origins)
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 
     @Override
